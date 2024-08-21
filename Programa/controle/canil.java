3978eb418f86ce2a2a47827399ceadaefc9c643d
Yago-LDT <129117046+Yago-LDT@users.noreply.github.com>
2024-08-21 18:41:47 -0300

    Create canil.java

---
+++ Programa/controle/canil.java
@@ -0,0 +1,215 @@
+package br.com.controle;
+
+public class Canil {
+    private String Nome;
+    private String Raça;
+    private String Peso;
+    private String Porte;
+    private String Origem;
+    private String Gênero;
+    private String Idade;
+    private String Vermifugação;
+    private String Antirrábica;
+    private String Leishmaniose;
+    private String Giárdia;
+    private String V8;
+    private String Gripe_Canina;
+    private String data_admissão;
+    private String Revacinacao;
+    private int ID;
+    private String Telefone;
+    private String Endereço;
+    private String Valor;
+    private int Cão_id;
+    private int Cliente_id;
+    private String data_venda;
+    private String data_doação;
+
+    public String getNome() {
+        return Nome;
+    }
+
+    public void setNome(String Nome) {
+        this.Nome = Nome;
+    }
+
+    public String getRaça() {
+        return Raça;
+    }
+
+    public void setRaça(String Raça) {
+        this.Raça = Raça;
+    }
+
+    public String getPeso() {
+        return Peso;
+    }
+
+    public void setPeso(String Peso) {
+        this.Peso = Peso;
+    }
+
+    public String getPorte() {
+        return Porte;
+    }
+
+    public void setPorte(String Porte) {
+        this.Porte = Porte;
+    }
+
+    public String getOrigem() {
+        return Origem;
+    }
+
+    public void setOrigem(String Origem) {
+        this.Origem = Origem;
+    }
+
+    public String getIdade() {
+        return Idade;
+    }
+
+    public void setIdade(String Idade) {
+        this.Idade = Idade;
+    }
+
+    
+    public String getData_admissão() {
+        return data_admissão;
+    }
+
+    public void setData_admissão(String data_admissão) {
+        this.data_admissão = data_admissão;
+    }
+
+    public String getRevacinacao() {
+        return Revacinacao;
+    }
+
+    public void setRevacinacao(String Revacinacao) {
+        this.Revacinacao = Revacinacao;
+    }
+
+    public int getID() {
+        return ID;
+    }
+
+    public void setID(int ID) {
+        this.ID = ID;
+    }
+
+    public String getTelefone() {
+        return Telefone;
+    }
+
+    public void setTelefone(String Telefone) {
+        this.Telefone = Telefone;
+    }
+
+    public String getEndereço() {
+        return Endereço;
+    }
+
+    public void setEndereço(String Endereço) {
+        this.Endereço = Endereço;
+    }
+
+    public String getValor() {
+        return Valor;
+    }
+
+    public void setValor(String Valor) {
+        this.Valor = Valor;
+    }
+
+    public String getVermifugação() {
+        return Vermifugação;
+    }
+
+    public void setVermifugação(String Vermifugação) {
+        this.Vermifugação = Vermifugação;
+    }
+
+    public String getAntirrábica() {
+        return Antirrábica;
+    }
+
+    public void setAntirrábica(String Antirrábica) {
+        this.Antirrábica = Antirrábica;
+    }
+
+    public String getLeishmaniose() {
+        return Leishmaniose;
+    }
+
+    public void setLeishmaniose(String Leishmaniose) {
+        this.Leishmaniose = Leishmaniose;
+    }
+
+    public String getV8() {
+        return V8;
+    }
+
+    public void setV8(String V8) {
+        this.V8 = V8;
+    }
+
+    public String getGripe_Canina() {
+        return Gripe_Canina;
+    }
+
+    public void setGripe_Canina(String Gripe_Canina) {
+        this.Gripe_Canina = Gripe_Canina;
+    }
+
+    public String getGênero() {
+        return Gênero;
+    }
+
+    public void setGênero(String Gênero) {
+        this.Gênero = Gênero;
+    }
+
+    public String getGiárdia() {
+        return Giárdia;
+    }
+
+    public void setGiárdia(String Giárdia) {
+        this.Giárdia = Giárdia;
+    }
+
+    public int getCão_id() {
+        return Cão_id;
+    }
+
+    public void setCão_id(int Cão_id) {
+        this.Cão_id = Cão_id;
+    }
+
+    public int getCliente_id() {
+        return Cliente_id;
+    }
+
+    public void setCliente_id(int Cliente_id) {
+        this.Cliente_id = Cliente_id;
+    }
+
+    public String getData_venda() {
+        return data_venda;
+    }
+
+    public void setData_venda(String data_venda) {
+        this.data_venda = data_venda;
+    }
+
+    public String getData_doação() {
+        return data_doação;
+    }
+
+    public void setData_doação(String data_doação) {
+        this.data_doação = data_doação;
+    }
+    
+    
+    
+}
